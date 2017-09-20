ea956ebab154947ed7980d96370d48f5a30737a8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-09-20 08:32:58 +0100

    Custom Post Templates UI
    refs https://github.com/TryGhost/Ghost/issues/9060, requires https://github.com/TryGhost/Ghost/pull/9073
    - add `{{gh-psm-template-select}}` component
      - allows selection of a custom template for a post if the active theme has custom templates
      - loads themes on render, only hitting the server if not already in the store
      - disables select if post slug matches a `post-*.hbs` or `page-*.hbs` template
    - adds `customTemplate` attr to `Post` model
    - adds `templates` attr to `Theme` model with CPs to pull out custom vs post/page override templates
    - add `.gh-select.disabled` styles to make disabled selects look visually disabled

--- app/models/setting.js
+++ app/models/setting.js
@@ -25,5 +25,9 @@ export default Model.extend(ValidationEngine, {
     password: attr('string'),
     slack: attr('slack-settings'),
     amp: attr('boolean'),
-    unsplash: attr('unsplash-settings', {defaultValue: {isActive: true}})
+    unsplash: attr('unsplash-settings', {
+        defaultValue() {
+            return {isActive: true};
+        }
+    })
 });
